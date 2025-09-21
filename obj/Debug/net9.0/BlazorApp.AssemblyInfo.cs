58c88460e0d302ef723878e6cc4c4e28d0521efc
masaljubicekof <masana311@gmail.com>
2025-09-21 20:50:44 +0200

    novo

--- obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e241b150db3dceae88fdacc428ca6b95d86e55bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe515563bb070d06f61b3d53514b34641afaf000")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
